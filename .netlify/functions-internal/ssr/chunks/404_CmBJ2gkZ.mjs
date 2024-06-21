0a9e436bc55476ffdf7fc638565fbdc857867893
Paul Guachichullca <paulguachichullca023@gmail.com>
2024-06-21 09:52:42 -0500

    con netlify

--- .netlify/functions-internal/ssr/chunks/404_CmBJ2gkZ.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/404_CM9X1WA9.mjs').then(n => n._);
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/index_LucJjsKN.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/index_8pDQ_g6G.mjs');
-
-export { page };
